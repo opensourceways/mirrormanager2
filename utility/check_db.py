6800ce39743d6ab16e31680086109ce56d39d836
zengchen1024 <chenzeng765@163.com>
2023-04-26 08:20:12 +0000

    update mm2_crawler

--- utility/check_db.py
+++ utility/check_db.py
@@ -2,54 +2,54 @@ import optparse
 import os
 import sys
 
-import mirrormanager2.lib
-
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.abspath(__file__)), '..'))
 
+import mirrormanager2.lib
+
 
 def print_db(session):
-    print("version....")
+    print("...version....")
     vers = mirrormanager2.lib.get_versions(session)
     for v in vers:
         print(vars(v))
 
-    print("arch....")
+    print("\n...arch....")
     arches = mirrormanager2.lib.get_arches(session)
     for v in arches:
         print(vars(v))
 
-    print("product....")
+    print("\n...product....")
     products = mirrormanager2.lib.get_products(session)
     for v in products:
         print(vars(v))
 
-    print("repository....")
+    print("\n...repository....")
     repositories = mirrormanager2.lib.get_repositories(session)
     for v in repositories:
         print(vars(v))
 
-    print("category....")
+    print("\n...category....")
     categories = mirrormanager2.lib.get_categories(session)
     for v in categories:
         print(vars(v))
 
-    print("directory....")
+    print("\n...directory....")
     directories = mirrormanager2.lib.get_directories(session)
     for v in directories:
         print(vars(v))
 
-    print("category directory....")
+    print("\n...category directory....")
     cds = mirrormanager2.lib.get_category_directory(session)
     for v in cds:
         print(vars(v))
 
-    print("file detail....")
+    print("\n...file detail....")
     fds = mirrormanager2.lib.get_file_details(session)
     for v in fds:
         print(vars(v))
 
-    print("host category dir....")
+    print("\n...host category dir....")
     hcds = mirrormanager2.lib.get_host_category_dirs(session)
     for v in hcds:
         print(vars(v))
@@ -69,7 +69,7 @@ def main():
 
     session = mirrormanager2.lib.create_session(config['DB_URL'])
 
-    print("Starting")
+    print("Starting\n")
 
     try:
         print_db(session)
@@ -80,7 +80,7 @@ def main():
     finally:
         session.close()
 
-    print("Ending")
+    print("\nEnding")
 
     return 0
 
